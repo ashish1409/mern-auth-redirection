1ddc7140953af441ddc2282ac43b2c44580bf9aa
ashish1409 <ashish.choudhary84@gmail.com>
2026-04-22 03:14:33 +0530

    add email varifaction condition

--- src/pages/EmailVarifaction.tsx
+++ src/pages/EmailVarifaction.tsx
@@ -1,7 +1,45 @@
+import axios from 'axios';
+import { useState,useEffect } from 'react';
 
-function EmailVerification() {
+const EmailVerification = () => {
+  const [loading, setLoading] = useState(true);
+  const [succes , setSuccess] = useState(false);
+
+  const urlParams = new URLSearchParams(window.location.search);
+  const tokenId = urlParams.get('token');
+  console.log(tokenId);
+
+useEffect(()=>{
+  const handleEmailVerification = async () => {
+    try {
+      const response = await axios.post('http://localhost:5000/api/auth/verify-email', { tokenId });
+      console.log(response.data);
+      setSuccess(true);
+    } catch (error) {
+      console.error('Email verification failed:', error);
+      setSuccess(false);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  if(!tokenId) {
+    console.error('Token is required');
+    return;
+  }
+  handleEmailVerification();
+}, [tokenId]);
+
   return (
-    <div>EmailVerification</div>
+    <>
+      {loading ? (
+        <div>Verifying your email...</div>
+      ) : succes ? (
+        <div>Email verified successfully! You can now log in.</div>
+      ) : (
+        <div>Email verification failed. Please try again.</div>
+      )}
+    </>
   )
 }
 
